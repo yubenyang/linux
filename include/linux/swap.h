f773988ebf59649114ed18af056c4ddc37376846
yuben <ybenyangmds@gmail.com>
2023-03-12 08:58:37 +0000

    increase SWAP_CLUSTER_MAX to 128UL

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -215,7 +215,7 @@ enum {
 	SWP_SCANNING	= (1 << 14),	/* refcount in scan_swap_map */
 };
 
-#define SWAP_CLUSTER_MAX 32UL
+#define SWAP_CLUSTER_MAX 128UL
 #define COMPACT_CLUSTER_MAX SWAP_CLUSTER_MAX
 
 /* Bit flag in swap_map */
